176880624f12fce66808836f1370701e9dc763e3
Catalin4Florin <catroa01@easv365.dk>
2025-04-10 09:42:47 +0200

    initial commit

---
+++ hjemmeside/src/components/NavBar.vue
@@ -0,0 +1,29 @@
+<template>
+  
+    <nav class="navBar">
+            <div class="search-filter p-4">
+          <label for=""> Search by title:</label>
+          <input type="text" class="text-black"
+          placeholder="Search by title"
+          v-model = "searchQuery"
+          />
+       </div>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/projects">Projects</RouterLink>
+    </nav>
+
+    
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router'
+
+</script>
+
+<style>
+.navBar{
+  display: flex;
+  justify-content: space-around;
+  padding: 1rem;
+}
+</style>
---
+++ hjemmeside/src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script >
+
+</script>
+
+<template>
+  <main>
+    <div class="home">
+      <h1 class="underline">This is home page</h1>
+      <p>Welcome to my homepage!</p>
+    </div>
+</main>
+</template>
